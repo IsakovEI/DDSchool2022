3274e0edfcb3e16b2db8b07df986e1f63288c624
isako <isako@DESKTOP-SPAQB20>
2022-10-30 20:06:25 +0300

    Add project files.

---
+++ Api/MapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Common;
+
+namespace Api
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Models.CreateUserModel, DAL.Entities.User>()
+                .ForMember(d => d.Id, m => m.MapFrom(s => Guid.NewGuid()))
+                .ForMember(d => d.PasswordHash, m => m.MapFrom(s => HashHelper.GetHash(s.Password)));
+
+            CreateMap<DAL.Entities.User, Models.UserModel>();
+        }
+    }
+}
---
+++ Common/HashHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Common
+{
+    public static class HashHelper
+    {
+        public static string GetHash(string input)
+        {
+            using(var sha = SHA256.Create())
+            {
+                var data = sha.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder();
+                foreach(var c in data)
+                {
+                    sb.Append(c.ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+        public static bool Verify(string input, string hash)
+        {
+            var comparer = StringComparer.OrdinalIgnoreCase;
+            return comparer.Compare(hash, GetHash(input)) == 0;
+        }
+    }
+}
